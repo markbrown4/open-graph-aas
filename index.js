316b22ee631475697eb738ed682ab02e6c2da3a3
Mark Brown <markbrown4@gmail.com>
2016-07-27 14:15:37 +0100

    enable cors

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser')
 var ogs = require('open-graph-scraper')
 
 var app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://(?:.+\.)?(tribes.dev|tribesapp.com)$");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
